5d175bb620d3456b5ed7e4e46f2961cdbbf10120
Dorian Yeager <yeagerd@objectcomputing.com>
2019-05-02 16:02:25 -0500

    refactoring

--- src/app.js
+++ src/app.js
@@ -1,60 +1,99 @@
 const express = require('express')
 const http = require('http')
-const socketio = require('socket.io')
-const socketClient = require('socket.io-client')
+const serverio = require('socket.io')
+const clientio = require('socket.io-client')
 const path = require('path')
 const flatCache = require('flat-cache')
 
 const app = express()
 const server = http.createServer(app)
-const serverio = socketio(server)
+const serverSocket = serverio(server)
 
 const hostConfiguration = require('./config/config')
-
 const localCache = flatCache.load('localCache')
 
-const fallBackToBootNode = (socketClient, hostConfiguration) => {
-    const bootNodeCount = hostConfiguration.bootNodes.length
-    let outboundSockets = []
-    let directory
-    let failCount = 0
-
-    for (i = 0; i < bootNodeCount; i++) {
-        outboundSockets[i] = socketClient.connect(path.join('http://', hostConfiguration.bootNodes[i]))
-
-        outboundSockets[i].on('connection', () => {
-            outboundSockets[i].emit('directory')
+const getDirectoryFromBootNode = (clientio, hostAddress) => {
+    let promise = new Promise((resolve, reject) => {
+        let outboundSocket = clientio.connect(path.join('http://', hostAddress), { forcenew: true })
+        outboundSocket.on('connection', (socket) => {
+            socket.emit('directory')
         })
 
-        outboundSockets[i].on('connect_error', (error) => {
-            failCount++
-            console.log('error: ' + error)
-            if (failCount === bootNodeCount) {
-                throw ('Unable to Connect to Boot Nodes')
-            }
+        outboundSocket.on('connect_error', (error) => {
+            reject('unable to connect')
         })
 
-        outboundSockets[i].on('directoryCast', (hostDirectory) => {
-            localCache.setKey('directory', hostDirectory)
-            localCache.save()
-            if (directory === undefined) {
-                directory = hostDirectory
-                standUpOutboundConnections(directory)
-            }
+        outboundSocket.on('directoryCast', (hostDirectory) => {
+            outboundSocket.close()
+            resolve(hostDirectory)
         })
+        setTimeout(() => {
+            reject('boot node timeout')
+        }, 5000)
+    })
+    return promise
+}
+
+const getDirectoryFromBootNodes = async (clientio, bootNodes) => {
+    for (let i = 0; i < bootNodes.length; i++) {
+        try {
+            directory = await getDirectoryFromBootNode(clientio, bootNodes[i])
+        } catch (e) {
+            console.log(e)
+        }
+        if (directory !== undefined) {
+            break;
+        }
+    }
+    return directory
+}
+
+const connectToPeer = (clientio, peerAddress) => {
+    let promise = new Promise((resolve, reject) => {
+        let peerSocket = clientio.connect(path.join('http://', peerAddress), { forcenew: true })
+        peerSocket.on('connection', (socket) => {
+            resolve(peerSocket)
+            //add message handlers
+        })
+        peerSocket.on('connect_error', (error) => {
+            reject('unable to connect to peer')
+        })
+        setTimeout(() => {
+            reject('peer timeout')
+        }, 5000)
+    })
+    return promise
+}
+
+const connectToPeers = async (clientio, bootNodes) => {
+    let peers = []
+
+    let directory = localCache.getKey('directory')
+    if (directory === undefined) {
+        directory = await getDirectoryFromBootNodes(clientio, bootNodes)
+    }
+
+    if (directory === undefined) {
+        reject('directory is unavailable')
+    } else {
+        localCache.setKey('directory', directory)
+        localCache.save()
+    }
+
+    while (peers.length < hostConfiguration.outboundCount) {
+        let peerIndex = Math.floor(Math.random() * directory.length) + 1
+        try {
+            let peer = await connectToPeer(clientio, directory[peerIndex])
+            peers.push(peer)
+        } catch (e) {
+            //noop
+        }
     }
 }
 
-let directory = localCache.getKey('directory')
-if (directory === undefined) {
-    try {
-        fallBackToBootNode(socketClient, hostConfiguration)
-    } catch (e) {
-        console.log(e)
-        server.close()
-    }
-} else {
-    standUpOutboundConnections(directory)
-}
+connectToPeers(clientio, hostConfiguration.bootNodes).then(() => { console.log('peers connected') }).catch((e) => {
+    console.log('Unable to locate a directory of peer nodes')
+    server.close()
+})
 
 server.listen(hostConfiguration.port)
