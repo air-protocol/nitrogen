1fadc1479e80d8a3d6c9f4714632bdffe50e104c
Dorian Yeager <yeagerd@objectcomputing.com>
2019-07-01 15:42:47 -0500

    adding tests and code for escrow creation

--- src/consumer/chain.js
+++ src/consumer/chain.js
@@ -1,8 +1,34 @@
 const stellar = require('stellar-sdk')
 const fetch = require('node-fetch')
 
-const initiateSettlement = async (secret, challengeStake, nativeAmount) => {
 
+const initiateSettlement = async (secret, challengeStake, nativeAmount) => {
+    const server = new stellar.Server('https://horizon-testnet.stellar.org');
+    const pair = stellar.Keypair.fromSecret(secret)
+    const escrowPair = stellar.Keypair.random()
+    const account = await server.loadAccount(pair.publicKey)
+
+    stellar.Network.useTestNetwork()
+
+    const txOptions = {
+        fee: await server.fetchBaseFee()
+    }
+
+    const total = txOptions.fee + challengeStake + nativeAmount
+    
+    const escrowAccountConfig = {
+        destination: escrowPair.publicKey,
+        startingBalance: total.toString()
+    }
+
+    let transaction = new stellar.TransactionBuilder(account, txOptions)
+        .addOperation(stellar.Operation.createAccount(escrowAccountConfig))
+        .setTimeout(stellar.TimeoutInfinite)
+        .build()
+
+    transaction.sign(pair)
+
+    await server.submitTransaction(transaction)
 }
 
 const transactionHistory = async (accountId) => {
