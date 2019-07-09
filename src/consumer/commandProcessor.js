c7f3af3d7b5450ec181c07d818cb2cfb4d9b8a1a
Jesse Hanner <hannerj@objectcomputing.com>
2019-07-09 13:22:41 -0500

    catching and logging errors

--- src/consumer/commandProcessor.js
+++ src/consumer/commandProcessor.js
@@ -3,7 +3,7 @@ const { buildMessage, sendMessage } = require('./consumerPeer')
 const { proposalSchema, negotiationSchema, proposalResolvedSchema, fulfillmentSchema } = require('../models/schemas')
 const { initiateSettlement, transactionHistory } = require('./chain')
 const hostConfiguration = require('../config/config')
-const logger = require('../logging')
+const logger = require('./clientLogging')
 
 const getKeyFromPreviousHash = (previousHash, proposal) => {
     let recipientKey = undefined
@@ -36,13 +36,13 @@ const processProposals = (proposals) => {
     console.clear()
     if (proposals.size) {
         proposals.forEach((proposal, requestId) => {
-            logger.info('---------------------------------')
-            logger.info('request: ' + requestId)
-            logger.info('offer asset: ' + proposal.body.offerAsset)
-            logger.info('offer amount: ' + proposal.body.offerAmount)
-            logger.info('request asset: ' + proposal.body.requestAsset)
-            logger.info('request amount: ' + proposal.body.requestAmount)
-            logger.info('---------------------------------')
+            console.log('---------------------------------')
+            console.log('request: ' + requestId)
+            console.log('offer asset: ' + proposal.body.offerAsset)
+            console.log('offer amount: ' + proposal.body.offerAmount)
+            console.log('request asset: ' + proposal.body.requestAsset)
+            console.log('request amount: ' + proposal.body.requestAmount)
+            console.log('---------------------------------')
         })
     } else {
         logger.warn('no proposals')
@@ -53,7 +53,7 @@ const processProposalResolved = async (param, proposals, keys) => {
     let resolveBody = JSON.parse(param)
     let proposal = proposals.get(resolveBody.requestId)
     if (!proposal) {
-        logger.warn("Unable to find proposal")
+        throw new Error("Unable to find proposal")
     }
     let resolution = buildMessage(resolveBody, keys, proposalResolvedSchema)
     resolution = await signMessage(resolution, keys)
@@ -64,7 +64,7 @@ const processProposalResolved = async (param, proposals, keys) => {
 const processNegotiationMessage = async (messageBody, proposal, keys, messageType) => {
     let recipientKey = getKeyFromPreviousHash(messageBody.previousHash, proposal)
     if (!recipientKey) {
-        logger.warn('Unable to match up hashes')
+        throw new Error('Unable to match up hashes')
     } else {
         try {
             let message = buildMessage(messageBody, keys, negotiationSchema)
@@ -73,7 +73,7 @@ const processNegotiationMessage = async (messageBody, proposal, keys, messageTyp
             message = await encryptMessage(message, recipientKey)
             sendMessage(messageType, message)
         } catch (e) {
-            logger.warn('unable to sign and encrypt: ' + e)
+            throw new Error('unable to sign and encrypt: ' + e)
         }
     }
     return copyMessage
@@ -83,30 +83,29 @@ const processFulfillment = async (param, proposals, keys) => {
     //previous hash is of acceptance
     let fulfillmentBody = JSON.parse(param)
     let proposal = proposals.get(fulfillmentBody.requestId)
-    if(! proposal) {
+    if (!proposal) {
         throw new Error('Unable to locate proposal')
     }
-    if (! proposal.resolution) {
+    if (!proposal.resolution) {
         throw new Error('Proposal is not resolved')
     }
     let acceptance = undefined
-    for(i = 0; i < proposal.acceptances.length; i++) {
+    for (i = 0; i < proposal.acceptances.length; i++) {
         if (proposal.acceptances[i].takerId === proposal.resolution.takerId) {
             acceptance = proposal.acceptances[i]
         }
     }
-    if (! acceptance) {
+    if (!acceptance) {
         throw new Error('Proposal did not resolve an acceptance')
     }
     let recipientKey
-    if(JSON.stringify(keys.publicKey) !== JSON.stringify(acceptance.publicKey))
-    {
+    if (JSON.stringify(keys.publicKey) !== JSON.stringify(acceptance.publicKey)) {
         recipientKey = acceptance.publicKey
     } else {
         recipientKey = getKeyFromPreviousHash(acceptance.body.previousHash, proposal)
     }
     if (!recipientKey) {
-        logger.warn('Unable to match up hashes')
+        throw new Error('Unable to match up hashes')
     } else {
         try {
             let message = buildMessage(fulfillmentBody, keys, fulfillmentSchema)
@@ -116,7 +115,7 @@ const processFulfillment = async (param, proposals, keys) => {
             sendMessage('fulfillment', message)
             proposal.fulfillments.push(copyMessage)
         } catch (e) {
-            logger.warn('unable to sign and encrypt: ' + e)
+            throw new Error('unable to sign and encrypt: ' + e)
         }
     }
 }
@@ -128,7 +127,7 @@ const processAcceptProposal = async (param, proposals, keys) => {
         let acceptanceMessage = await processNegotiationMessage(acceptBody, proposal, keys, 'accept')
         proposal.acceptances.push(acceptanceMessage)
     } else {
-        logger.warn('Unable to match acceptance to original proposal')
+        throw new Error('Unable to match acceptance to original proposal')
     }
 }
 
@@ -167,101 +166,98 @@ const processSettleProposal = async (param, proposals) => {
 const processCounterOffer = async (param, proposals, keys) => {
     let counterOfferBody = JSON.parse(param)
     let proposal = proposals.get(counterOfferBody.requestId)
-    if (proposal) {
-        let counterOfferMessage = await processNegotiationMessage(counterOfferBody, proposal, keys, 'counterOffer')
-        proposal.counterOffers.push(counterOfferMessage)
-    } else {
-        logger.warn('Unable to match counter offer to a proposal')
+    if (!proposal) {
+        throw new Error('Unable to match counter offer to a proposal')
     }
+    let counterOfferMessage = await processNegotiationMessage(counterOfferBody, proposal, keys, 'counterOffer')
+    proposal.counterOffers.push(counterOfferMessage)
 }
 
 const processCounterOffers = (param, proposals) => {
     let counteredProposal = proposals.get(param)
-    if (counteredProposal) {
-        counteredProposal.counterOffers.forEach((counterOffer) => {
-            logger.info('---------------------------------')
-            logger.info('request: ' + counterOffer.body.requestId)
-            logger.info('taker id: ' + counterOffer.body.takerId)
-            logger.info('offer asset: ' + counterOffer.body.offerAsset)
-            logger.info('offer amount: ' + counterOffer.body.offerAmount)
-            logger.info('request asset: ' + counterOffer.body.requestAsset)
-            logger.info('request amount: ' + counterOffer.body.requestAmount)
-            logger.info('---------------------------------')
-        })
-    } else {
-        logger.info('proposal not found')
+    if (!counteredProposal) {
+        throw new Error('proposal not found')
     }
+    counteredProposal.counterOffers.forEach((counterOffer) => {
+        console.log('---------------------------------')
+        console.log('request: ' + counterOffer.body.requestId)
+        console.log('taker id: ' + counterOffer.body.takerId)
+        console.log('offer asset: ' + counterOffer.body.offerAsset)
+        console.log('offer amount: ' + counterOffer.body.offerAmount)
+        console.log('request asset: ' + counterOffer.body.requestAsset)
+        console.log('request amount: ' + counterOffer.body.requestAmount)
+        console.log('---------------------------------')
+    })
 }
 
 const processOfferHistory = (param, proposals) => {
     let proposal = proposals.get(param)
-    if (proposal) {
-        logger.info('---------------------------------')
-        logger.info('Original Proposal')
-        logger.info('from public key: ' + JSON.stringify(proposal.publicKey))
-        logger.info('request: ' + proposal.body.requestId)
-        logger.info('maker id: ' + proposal.body.makerId)
-        logger.info('offer asset: ' + proposal.body.offerAsset)
-        logger.info('offer amount: ' + proposal.body.offerAmount)
-        logger.info('request asset: ' + proposal.body.requestAsset)
-        logger.info('request amount: ' + proposal.body.requestAmount)
-        logger.info('---------------------------------')
-        proposal.counterOffers.forEach((counterOffer) => {
-            logger.info('---------------------------------')
-            logger.info('Counter Offer')
-            logger.info('from public key: ' + JSON.stringify(counterOffer.publicKey))
-            logger.info('request: ' + counterOffer.body.requestId)
-            logger.info('maker id: ' + counterOffer.body.makerId)
-            logger.info('taker id: ' + counterOffer.body.takerId)
-            logger.info('offer asset: ' + counterOffer.body.offerAsset)
-            logger.info('offer amount: ' + counterOffer.body.offerAmount)
-            logger.info('request asset: ' + counterOffer.body.requestAsset)
-            logger.info('request amount: ' + counterOffer.body.requestAmount)
-            logger.info('---------------------------------')
-        })
-        proposal.acceptances.forEach((acceptance) => {
-            logger.info('---------------------------------')
-            logger.info('Acceptance')
-            logger.info('from public key: ' + JSON.stringify(acceptance.publicKey))
-            logger.info('request: ' + acceptance.body.requestId)
-            logger.info('maker id: ' + acceptance.body.makerId)
-            logger.info('taker id: ' + acceptance.body.takerId)
-            logger.info('offer asset: ' + acceptance.body.offerAsset)
-            logger.info('offer amount: ' + acceptance.body.offerAmount)
-            logger.info('request asset: ' + acceptance.body.requestAsset)
-            logger.info('request amount: ' + acceptance.body.requestAmount)
-            logger.info('---------------------------------')
-        })
-        proposal.fulfillments.forEach((fulfillment) => {
-            logger.info('---------------------------------')
-            logger.info('Fulfillment')
-            logger.info('from public key: ' + JSON.stringify(fulfillment.publicKey))
-            logger.info('request: ' + fulfillment.body.requestId)
-            logger.info('maker id: ' + fulfillment.body.makerId)
-            logger.info('taker id: ' + fulfillment.body.takerId)
-            logger.info('message: ' + fulfillment.body.message)
-            logger.info('fulfullment: ' + JSON.stringify(fulfillment.body.fulfillment))
-            logger.info('---------------------------------')
-        })
-        if (proposal.resolution) {
-            logger.info('---------------------------------')
-            logger.info('Proposal resolved accepting taker id: ' + proposal.resolution.takerId)
-        }
-    } else {
-        logger.warn('proposal not found')
+    if (!proposal) {
+        throw new Error('proposal not found')
+    }
+    console.log('---------------------------------')
+    console.log('Original Proposal')
+    console.log('from public key: ' + JSON.stringify(proposal.publicKey))
+    console.log('request: ' + proposal.body.requestId)
+    console.log('maker id: ' + proposal.body.makerId)
+    console.log('offer asset: ' + proposal.body.offerAsset)
+    console.log('offer amount: ' + proposal.body.offerAmount)
+    console.log('request asset: ' + proposal.body.requestAsset)
+    console.log('request amount: ' + proposal.body.requestAmount)
+    console.log('---------------------------------')
+    proposal.counterOffers.forEach((counterOffer) => {
+        console.log('---------------------------------')
+        console.log('Counter Offer')
+        console.log('from public key: ' + JSON.stringify(counterOffer.publicKey))
+        console.log('request: ' + counterOffer.body.requestId)
+        console.log('maker id: ' + counterOffer.body.makerId)
+        console.log('taker id: ' + counterOffer.body.takerId)
+        console.log('offer asset: ' + counterOffer.body.offerAsset)
+        console.log('offer amount: ' + counterOffer.body.offerAmount)
+        console.log('request asset: ' + counterOffer.body.requestAsset)
+        console.log('request amount: ' + counterOffer.body.requestAmount)
+        console.log('---------------------------------')
+    })
+    proposal.acceptances.forEach((acceptance) => {
+        console.log('---------------------------------')
+        console.log('Acceptance')
+        console.log('from public key: ' + JSON.stringify(acceptance.publicKey))
+        console.log('request: ' + acceptance.body.requestId)
+        console.log('maker id: ' + acceptance.body.makerId)
+        console.log('taker id: ' + acceptance.body.takerId)
+        console.log('offer asset: ' + acceptance.body.offerAsset)
+        console.log('offer amount: ' + acceptance.body.offerAmount)
+        console.log('request asset: ' + acceptance.body.requestAsset)
+        console.log('request amount: ' + acceptance.body.requestAmount)
+        console.log('---------------------------------')
+    })
+    proposal.fulfillments.forEach((fulfillment) => {
+        console.log('---------------------------------')
+        console.log('Fulfillment')
+        console.log('from public key: ' + JSON.stringify(fulfillment.publicKey))
+        console.log('request: ' + fulfillment.body.requestId)
+        console.log('maker id: ' + fulfillment.body.makerId)
+        console.log('taker id: ' + fulfillment.body.takerId)
+        console.log('message: ' + fulfillment.body.message)
+        console.log('fulfullment: ' + JSON.stringify(fulfillment.body.fulfillment))
+        console.log('---------------------------------')
+    })
+    if (proposal.resolution) {
+        console.log('---------------------------------')
+        console.log('Proposal resolved accepting taker id: ' + proposal.resolution.takerId)
     }
 }
 
 const processTransactionHistory = async (accountId) => {
     const records = await transactionHistory(accountId)
     records.forEach((item) => {
-        logger.info('\n' + 'Source Account: ' + item.source_account)
-        logger.info('Source Account Sequence: ' + item.source_account_sequence)
-        logger.info('Created At: ' + item.created_at)
-        logger.info('Memo: ' + item.memo)
-        logger.info('Successful: ' + item.successful)
-        logger.info('Fee Paid: ' + item.fee_paid)
-        logger.info('Ledger number: ' + item.ledger)
+        console.log('\n' + 'Source Account: ' + item.source_account)
+        console.log('Source Account Sequence: ' + item.source_account_sequence)
+        console.log('Created At: ' + item.created_at)
+        console.log('Memo: ' + item.memo)
+        console.log('Successful: ' + item.successful)
+        console.log('Fee Paid: ' + item.fee_paid)
+        console.log('Ledger number: ' + item.ledger)
     })
 }
 
