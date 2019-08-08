c95fc4ea45a322c9ba445278a76612ae66755767
Dorian Yeager <yeagerd@objectcomputing.com>
2019-08-08 12:00:40 -0500

    consumerSignareRequired rejects proposals that do not resolve an acceptance

--- tests/consumer/consumerMessageHandler.test.js
+++ tests/consumer/consumerMessageHandler.test.js
@@ -1029,3 +1029,41 @@ test('consumerSignatureRequiredHandler rejects bad signatures', async () => {
     expect(logger.warn).toBeCalled()
     expect(logger.warn.mock.calls[0][0]).toMatch("unable to process inbound signatureRequired: Error: Couldn't verify message signature")
 })
+
+test('consumerSignatureRequired rejects proposals that did not resolve with an acceptance', async () => {
+    //Assemble
+    verifyMessage.mockReturnValue(new Promise((resolve, reject) => { resolve(true) }))
+    messageSeen.mockReturnValue(false)
+    proposalResolvedWithAcceptance.mockReturnValue(false)
+
+    const peerMessage = {
+        'recipientKey': keys.publicKey.toString('hex'),
+        'uuid': 'someid',
+        'body': {
+            'requestId': 'abc123'
+        }
+    }
+
+    const decryptedMessage = JSON.parse(JSON.stringify(peerMessage))
+    decryptMessage.mockReturnValue(decryptedMessage)
+
+    const proposal = {
+        'uuid': 'someid',
+        'body': {
+            'requestId': 'abc123'
+        },
+        'acceptances': [],
+        'fulfillments': []
+    }
+    const proposals = new Map()
+    proposals.set('abc123', proposal)
+
+
+    //Action
+    await consumerSignatureRequiredHandler(peerMessage, proposals, keys)
+
+    //Assert
+    expect(logger.warn).toBeCalled()
+    expect(logger.warn.mock.calls[0][0]).toMatch("unable to locate proposal that resolved with acceptance for inbound signatureRequired")
+    expect(proposal.signatureRequired).toBe(undefined)
+})
